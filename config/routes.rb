b041f28bbfc87a343c719517732fb16eeb064ae7
Olivia <oliviatmanalastas@gmail.com>
2022-09-12 10:14:34 -0700

    Done with assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show]
+  resources :articles, only: [:index]
+
+  get '/articles/:id', to: 'articles#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
